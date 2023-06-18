cd13e2b5dd0f6877fd8442610cfc56530dc4466a
Felipe S Bezerra <felipecrf220@gmail.com>
2023-06-18 12:15:58 -0300

    fix: aparecer sessão expirada apenas quando houver um token em localstorage

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../service/auth.service';
 import { inject } from '@angular/core';
+import { SnackbarService } from '../service/snackbar.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService: AuthService = inject(AuthService);
--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -4,6 +4,7 @@ import { Credenciais } from '../models/credenciais';
 import { HttpClient, HttpResponse } from '@angular/common/http';
 import { API_CONFIG } from '../config/api.config';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { SnackbarService } from './snackbar.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class AuthService {
 
 jwtService: JwtHelperService = new JwtHelperService();
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private snackBar: SnackbarService) { }
 
   authenticate(credenciais: Credenciais): Observable<HttpResponse<string>> {
     return this.http.post(`${API_CONFIG.baseUrl}/login`, credenciais, {
@@ -28,6 +29,9 @@ jwtService: JwtHelperService = new JwtHelperService();
   isAuthenticated(): boolean {
     let token = localStorage.getItem('token');
     if (token != null) {
+      if (this.jwtService.isTokenExpired(token)) {
+        this.snackBar.showMessage('Sessão expirada', true, 8000);
+      }
       return !this.jwtService.isTokenExpired(token);
     }
     return false;
