8b60b3412c1b475d06ad0784d5508d7227d406c2
Felipe S Bezerra <felipecrf220@gmail.com>
2023-06-25 14:38:26 -0300

    validacao de formularios para chamado-create

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-chamado-create',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class ChamadoCreateComponent {
 
+  titulo: FormControl = new FormControl(null, [Validators.required]);
+  prioridade: FormControl = new FormControl(null, [Validators.required]);
+  status: FormControl = new FormControl(null, [Validators.required]);
+  tecnico: FormControl = new FormControl(null, [Validators.required]);
+  cliente: FormControl = new FormControl(null, [Validators.required]);
+  descricao: FormControl = new FormControl(null, [Validators.required]);
+
+  validarCompos(): boolean {
+    return this.titulo.valid 
+      && this.prioridade.valid
+      && this.status.valid
+      && this.tecnico.valid
+      && this.cliente.valid
+      && this.descricao.valid
+  }
 }
