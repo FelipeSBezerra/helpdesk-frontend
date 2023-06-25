92acd4bd862edffaa6817c9f7aa1397a460f1857
Felipe S Bezerra <felipecrf220@gmail.com>
2023-06-25 16:15:43 -0300

    servico para criar chamado implementado

--- src/app/components/chamado/chamado-create/chamado-create.component.ts
+++ src/app/components/chamado/chamado-create/chamado-create.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Chamado } from 'src/app/models/chamado';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ChamadoService } from 'src/app/service/chamado.service';
+import { ClienteService } from 'src/app/service/cliente.service';
+import { SnackbarService } from 'src/app/service/snackbar.service';
+import { TecnicoService } from 'src/app/service/tecnico.service';
 
 @Component({
   selector: 'app-chamado-create',
@@ -8,12 +16,68 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class ChamadoCreateComponent {
 
+  tecnicos: Tecnico[] = [];
+  clientes: Cliente[] = [];
+
+  chamado: Chamado = {
+    prioridade: '',
+    status: '',
+    titulo: '',
+    observacoes: '',
+    tecnico: '',
+    cliente: '',
+    nomeCliente: '',
+    nomeTecnico: '',
+  }
+
   titulo: FormControl = new FormControl(null, [Validators.required]);
   prioridade: FormControl = new FormControl(null, [Validators.required]);
   status: FormControl = new FormControl(null, [Validators.required]);
   tecnico: FormControl = new FormControl(null, [Validators.required]);
   cliente: FormControl = new FormControl(null, [Validators.required]);
-  descricao: FormControl = new FormControl(null, [Validators.required]);
+  observacoes: FormControl = new FormControl(null, [Validators.required]);
+
+  constructor (
+    private tecnicoService: TecnicoService, 
+    private clienteService: ClienteService,
+    private chamadoService: ChamadoService,
+    private snackBar: SnackbarService,
+    private router: Router,
+  ) {
+    this.findAllTecnicos();
+    this.findAllClientes();
+  }
+
+  create(): void {
+    if (this.validarCompos()){
+      this.chamadoService.create(this.chamado).subscribe(resposta => {
+        this.snackBar.showMessage('Chamado criado com sucesso', false);
+        this.router.navigate(['/chamados']);
+      }, ex => {
+        if (ex.error.errors) {
+          ex.error.errors.forEach((element: { message: string; }) => {
+            this.snackBar.showMessage(element.message, true, 5000)
+          });
+        } else {
+          this.snackBar.showMessage(ex.error.message, true, 5000);
+        }
+      })
+    } else {
+      this.snackBar.showMessage('Digite dados válidos antes de salvar', true, 5000);
+    }
+  }
+
+  findAllTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  findAllClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
 
   validarCompos(): boolean {
     return this.titulo.valid 
@@ -21,6 +85,6 @@ export class ChamadoCreateComponent {
       && this.status.valid
       && this.tecnico.valid
       && this.cliente.valid
-      && this.descricao.valid
+      && this.observacoes.valid
   }
 }
